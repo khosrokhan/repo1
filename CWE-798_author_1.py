d774bbd90d256c8b2f9650c9fce39432c4c71eec
khosro <goudarzi91@gmail.com>
2023-02-21 08:44:17 +0330

    delete files

--- CWE-798_author_1.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-
-def getDBConnection():
-    '''
-    return a mysql database connection
-    '''
-    return mysql.connector.connect(user='user', password='password', host='host', database='database')
